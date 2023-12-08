d6e30a948b5695ebae65f7b492c39148947fbf3d
Darlan Jr <kelgaxd@gmail.com>
2023-12-08 11:40:05 -0300

    remove dist

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare function add(a: number, b: number): number;
-
-export { add };
--- dist/index.mjs
+++
@@ -1,9 +0,0 @@
-// src/index.ts
-function add(a, b) {
-  return a + b;
-}
-console.log(add(3, 5));
-export {
-  add
-};
-//# sourceMappingURL=index.mjs.map

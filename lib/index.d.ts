c88593425421dbcee638cb182e16587c38f955fd
Darlan Jr <kelgaxd@gmail.com>
2023-12-09 22:50:11 -0300

    update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,15 @@
+export type RestToJSONRPCInput = {
+    path: string;
+    id: string;
+    params: any;
+};
+export type JSONRPCReturn = {
+    method: any;
+    jsonrpc: string;
+    params: any;
+    id: string;
+};
 export declare const getChannel: () => string;
 export declare const getRepository: () => string;
 export declare const sum: (a: number, b: number) => number;
+export declare const restToJSONRPC: (body: RestToJSONRPCInput) => JSONRPCReturn;
